5a603be897f1cede3dc8b11c49ee15b70c8d4f4c
Eric van der Velde <e.vandervelde@vwt.digital>
2020-04-08 11:14:33 +0200

    DAT-2358. Added CloudKMS revoke function

--- scripts/revoke_hpa.py
+++ scripts/revoke_hpa.py
@@ -33,6 +33,50 @@ def set_policy(project_id, policy):
     return policy
 
 
+def update_cloudkms_policy(projectId):
+    credentials = GoogleCredentials.get_application_default()
+
+    kms_service = googleapiclient.discovery.build('cloudkms', 'v1', credentials=credentials)
+    project = 'projects/{}'.format(projectId)
+    locations = kms_service.projects().locations().list(name=project).execute()
+
+    for location in locations['locations']:
+        keyrings = kms_service.projects().locations().keyRings().list(parent=location['name']).execute()
+
+        if 'keyRings' in keyrings:
+            for keyRing in keyrings['keyRings']:
+
+                kms_policy = kms_service.projects().locations().keyRings().getIamPolicy(resource=keyRing['name']).execute()
+                kms_policy_updated = False
+                if 'bindings' in kms_policy:
+                    for binding in kms_policy['bindings']:
+                        if 'members' in binding:
+                            for member in binding['members']:
+                                if member.startswith('user'):
+                                    print('Remove member {} from policy {}'.format(member, kms_policy))
+                                    modify_policy_remove_member(kms_policy, binding['role'], member)
+                                    kms_policy_updated = True
+                if kms_policy_updated:
+                    print(kms_service.projects().locations().keyRings()
+                                     .setIamPolicy(resource=keyRing['name'], body={'policy': kms_policy}).execute())
+
+
+def update_iam_policy(projectId):
+    iam_policy = get_policy(projectId)
+    modified = False
+
+    for binding in iam_policy['bindings']:
+        for member in reversed(binding['members']):
+            if 'user:' in member:
+                modified = True
+                iam_policy = modify_policy_remove_member(iam_policy, binding['role'], member)
+                print("Removed [{}],[{}]".format(member, binding['role']))
+
+    if modified:
+        print("New Policy {}".format(iam_policy))
+        set_policy(pr['projectId'], iam_policy)
+
+
 if len(sys.argv) > 1:
     parent_id = sys.argv[1]
 
@@ -40,32 +84,13 @@ if len(sys.argv) > 1:
     service = googleapiclient.discovery.build('cloudresourcemanager', 'v1', credentials=credentials)
 
     request = service.projects().list(filter="parent.id={}".format(parent_id))
-    print(request.to_json())
+
     while request is not None:
         response = request.execute()
         print(response)
 
         for pr in response.get('projects', []):
-            print(pr['projectId'])
-
-#            kms_service = googleapiclient.discovery.build('cloudkms', 'v1', credentials=credentials)
-#            keyring = 'projects/{}/locations/*/keyRings/*'.format(pr['projectId'])
-#            print(keyring)
-#            kms_policy = kms_service.projects().locations().keyRings().list(resource=keyring).execute()
-#            print(kms_policy)
-
-            policy = get_policy(pr['projectId'])
-            modified = False
-
-            for binding in policy['bindings']:
-                for member in reversed(binding['members']):
-                    if 'user:' in member:
-                        modified = True
-                        policy = modify_policy_remove_member(policy, binding['role'], member)
-                        print("Removed [{}],[{}]".format(member, binding['role']))
-
-            if modified:
-                print("New Policy {}".format(policy))
-                set_policy(pr['projectId'], policy)
+            update_iam_policy(pr['projectId'])
+            update_cloudkms_policy(pr['projectId'])
 
             request = service.projects().list_next(previous_request=request, previous_response=response)
