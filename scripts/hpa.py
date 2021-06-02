150dff5a41c7887dd344812992a3ec174b89e979
Daimy van Rhenen <d.vanrhenen@vwt.digital>
2021-06-02 17:28:58 +0200

    Added usage of forbidden roles in HPA request [skip ci]. DAT-6757

--- scripts/hpa.py
+++ scripts/hpa.py
@@ -1,26 +1,31 @@
-import git
-import json
 import argparse
+import json
 import logging
-
+import sys
 from os import path
 from pprint import pformat
+
+import git
 from googleapiclient import discovery
 from oauth2client.client import GoogleCredentials
 
-logging.basicConfig(level=logging.INFO, format='%(levelname)7s: %(message)s')
+logging.basicConfig(level=logging.INFO, format="%(levelname)7s: %(message)s")
 
 
 def get_kms_policy(service, project_id, location_id, key_ring_id):
     """Gets iam policy for a keyring."""
 
-    keyring = 'projects/{}/locations/{}/keyRings/{}'.format(
-        project_id,
-        location_id,
-        key_ring_id)
+    keyring = "projects/{}/locations/{}/keyRings/{}".format(
+        project_id, location_id, key_ring_id
+    )
 
-    policy = service.projects().locations().keyRings().getIamPolicy(
-        resource=keyring).execute()
+    policy = (
+        service.projects()
+        .locations()
+        .keyRings()
+        .getIamPolicy(resource=keyring)
+        .execute()
+    )
 
     return policy
 
@@ -28,14 +33,17 @@ def get_kms_policy(service, project_id, location_id, key_ring_id):
 def set_kms_policy(service, project_id, location_id, key_ring_id, policy):
     """Sets iam policy for a keyring."""
 
-    keyring = 'projects/{}/locations/{}/keyRings/{}'.format(
-        project_id,
-        location_id,
-        key_ring_id)
+    keyring = "projects/{}/locations/{}/keyRings/{}".format(
+        project_id, location_id, key_ring_id
+    )
 
-    policy = service.projects().locations().keyRings().setIamPolicy(
-        resource=keyring,
-        body={'policy': policy}).execute()
+    policy = (
+        service.projects()
+        .locations()
+        .keyRings()
+        .setIamPolicy(resource=keyring, body={"policy": policy})
+        .execute()
+    )
 
     return policy
 
@@ -43,8 +51,9 @@ def set_kms_policy(service, project_id, location_id, key_ring_id, policy):
 def get_stg_policy(service, bucket):
     """Gets IAM policy for a project."""
 
-    policy = service.buckets().getIamPolicy(
-        bucket=bucket.replace('gs://', '')).execute()
+    policy = (
+        service.buckets().getIamPolicy(bucket=bucket.replace("gs://", "")).execute()
+    )
 
     return policy
 
@@ -52,9 +61,11 @@ def get_stg_policy(service, bucket):
 def set_stg_policy(service, bucket, policy):
     """Sets IAM policy for a project."""
 
-    policy = service.buckets().setIamPolicy(
-        bucket=bucket.replace('gs://', ''),
-        body=policy).execute()
+    policy = (
+        service.buckets()
+        .setIamPolicy(bucket=bucket.replace("gs://", ""), body=policy)
+        .execute()
+    )
 
     return policy
 
@@ -63,15 +74,15 @@ def modify_policy(policy, role, member):
     """Modify policy"""
 
     bindings = []
-    if 'bindings' in policy.keys():
-        bindings = policy['bindings']
+    if "bindings" in policy.keys():
+        bindings = policy["bindings"]
     members = [member]
 
     new_binding = dict()
-    new_binding['role'] = role
-    new_binding['members'] = members
+    new_binding["role"] = role
+    new_binding["members"] = members
     bindings.append(new_binding)
-    policy['bindings'] = bindings
+    policy["bindings"] = bindings
 
     return policy
 
@@ -79,9 +90,7 @@ def modify_policy(policy, role, member):
 def get_iam_policy(service, project_id):
     """Gets IAM policy for a project."""
 
-    policy = service.projects().getIamPolicy(
-        resource=project_id,
-        body={}).execute()
+    policy = service.projects().getIamPolicy(resource=project_id, body={}).execute()
 
     return policy
 
@@ -89,9 +98,11 @@ def get_iam_policy(service, project_id):
 def set_iam_policy(service, project_id, policy):
     """Sets IAM policy for a project."""
 
-    policy = service.projects().setIamPolicy(
-        resource=project_id,
-        body={'policy': policy}).execute()
+    policy = (
+        service.projects()
+        .setIamPolicy(resource=project_id, body={"policy": policy})
+        .execute()
+    )
 
     return policy
 
@@ -102,9 +113,8 @@ def make_service(service):
     credentials = GoogleCredentials.get_application_default()
 
     service = discovery.build(
-        service, 'v1',
-        credentials=credentials,
-        cache_discovery=False)
+        service, "v1", credentials=credentials, cache_discovery=False
+    )
 
     return service
 
@@ -112,23 +122,23 @@ def make_service(service):
 def git_changed_files(project_id):
     """Returns commit info for the last commmit in the current repo."""
 
-    repo = git.Repo('')
+    repo = git.Repo("")
     branch = str(repo.active_branch)
 
     files = []
 
-    if branch == 'develop':
-        last_commit = list(repo.iter_commits(paths='config/{}'.format(project_id)))[0]
+    if branch == "develop":
+        last_commit = list(repo.iter_commits(paths="config/{}".format(project_id)))[0]
         files = [file for file in last_commit.stats.files.keys() if project_id in file]
 
-    elif branch == 'master':
+    elif branch == "master":
         headcommit = repo.head.commit
         while True:
             headcommit = headcommit.parents[0]
             if len(headcommit.parents) != 1:
                 break
 
-        last_commits = list(repo.iter_commits(rev='{}..{}'.format(headcommit, branch)))
+        last_commits = list(repo.iter_commits(rev="{}..{}".format(headcommit, branch)))
 
         for commit in last_commits:
             for file in commit.stats.files.keys():
@@ -141,10 +151,16 @@ def git_changed_files(project_id):
 def parse_args():
     """A simple function to parse command line arguments."""
 
-    parser = argparse.ArgumentParser(description='Grant high privilege access requests')
-    parser.add_argument('-p', '--project-id',
-                        required=True,
-                        help='name of the GCP project')
+    parser = argparse.ArgumentParser(description="Grant high privilege access requests")
+    parser.add_argument(
+        "-p", "--project-id", required=True, help="name of the GCP project"
+    )
+    parser.add_argument(
+        "-r",
+        "--forbidden-roles",
+        required=False,
+        help="list of forbidden roles separated by comma",
+    )
     return parser.parse_args()
 
 
@@ -155,51 +171,79 @@ def main(args):
     for file in files:
 
         if not path.exists(file):
-            logging.info('File {} does not exist'.format(file))
+            logging.info("File {} does not exist".format(file))
             continue
 
-        logging.info('Changed file: {}'.format(file))
+        logging.info("Changed file: {}".format(file))
 
         with open(file) as json_file:
             hpa_request = json.load(json_file)
 
-        logging.info('Processing hpa request:')
+        logging.info("Processing hpa request:")
         logging.info(pformat(hpa_request))
 
-        for access_request in hpa_request.get('operational_access', []):
+        for access_request in hpa_request.get("operational_access", []):
 
-            for permission in access_request.get('odrlPolicy', {}).get('permission', []):
+            for permission in access_request.get("odrlPolicy", {}).get(
+                "permission", []
+            ):
 
-                if 'cloudkms' in permission['action']:
+                if args.forbidden_roles and permission[
+                    "action"
+                ] in args.forbidden_roles.split(","):
+                    print(
+                        f"Role '{permission['action']}' is a basic role and is forbidden to use, "
+                        f"please define either a predefined or custom role. "
+                        f"See https://cloud.google.com/iam/docs/understanding-roles"
+                    )
+                    sys.exit(1)
 
-                    kms_service = make_service('cloudkms')
-                    kms_policy = get_kms_policy(kms_service, permission['target'], permission['location'], permission['keyring'])
-                    new_kms_policy = modify_policy(kms_policy, permission['action'], permission['assignee'])
-                    set_kms_policy(permission['target'], permission['location'], permission['keyring'], new_kms_policy)
+                if "cloudkms" in permission["action"]:
 
-                    logging.info('Set new kms policy bindings:')
+                    kms_service = make_service("cloudkms")
+                    kms_policy = get_kms_policy(
+                        kms_service,
+                        permission["target"],
+                        permission["location"],
+                        permission["keyring"],
+                    )
+                    new_kms_policy = modify_policy(
+                        kms_policy, permission["action"], permission["assignee"]
+                    )
+                    set_kms_policy(
+                        permission["target"],
+                        permission["location"],
+                        permission["keyring"],
+                        new_kms_policy,
+                    )
+
+                    logging.info("Set new kms policy bindings:")
                     logging.info(pformat(new_kms_policy))
 
-                elif permission['target'].startswith('gs://'):
+                elif permission["target"].startswith("gs://"):
 
-                    stg_service = make_service('storage')
-                    stg_policy = get_stg_policy(stg_service, permission['target'])
-                    new_stg_policy = modify_policy(stg_policy, permission['action'], permission['assignee'])
-                    set_stg_policy(stg_service, permission['target'], new_stg_policy)
+                    stg_service = make_service("storage")
+                    stg_policy = get_stg_policy(stg_service, permission["target"])
+                    new_stg_policy = modify_policy(
+                        stg_policy, permission["action"], permission["assignee"]
+                    )
+                    set_stg_policy(stg_service, permission["target"], new_stg_policy)
 
-                    logging.info('Set new storage policy bindings:')
+                    logging.info("Set new storage policy bindings:")
                     logging.info(pformat(new_stg_policy))
 
                 else:
 
-                    crm_service = make_service('cloudresourcemanager')
-                    iam_policy = get_iam_policy(crm_service, permission['target'])
-                    new_iam_policy = modify_policy(iam_policy, permission['action'], permission['assignee'])
-                    set_iam_policy(crm_service, permission['target'], new_iam_policy)
+                    crm_service = make_service("cloudresourcemanager")
+                    iam_policy = get_iam_policy(crm_service, permission["target"])
+                    new_iam_policy = modify_policy(
+                        iam_policy, permission["action"], permission["assignee"]
+                    )
+                    set_iam_policy(crm_service, permission["target"], new_iam_policy)
 
-                    logging.info('Set new project iam policy bindings:')
-                    logging.info(pformat(new_iam_policy.get('bindings')))
+                    logging.info("Set new project iam policy bindings:")
+                    logging.info(pformat(new_iam_policy.get("bindings")))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main(parse_args())
